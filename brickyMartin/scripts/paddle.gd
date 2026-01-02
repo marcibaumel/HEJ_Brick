d8e3d3acac5fd08eda4294ea99d599eafe5f2b93
Baumel Márton <marcibaumel@gmail.com>
2026-01-02 17:01:16 +0100

    optimaze platform

--- brickyMartin/scripts/paddle.gd
+++ brickyMartin/scripts/paddle.gd
@@ -1,16 +1,26 @@
 extends CharacterBody2D
 
-
 const SPEED = 1000.0
+var paddleVelocity := Vector2.ZERO
+var lockedYAxis: float
+
+func _ready() -> void:
+	lockedYAxis = global_position.y
 
 func _physics_process(delta: float) -> void:
+	var prev_pos = global_position
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0.0:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0.0, SPEED)
 
+	velocity.y = 0.0  # important: don't accumulate y velocity
 	move_and_slide()
+
+	# hard lock y position
+	global_position.y = lockedYAxis
+
+	paddleVelocity = (global_position - prev_pos) / delta
+	paddleVelocity.y = 0
