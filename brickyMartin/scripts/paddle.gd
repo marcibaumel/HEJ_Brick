0d52acdc2f0e682ccd7aa3b639eed00ea8e50ae3
Baumel Márton <marcibaumel@gmail.com>
2026-01-10 21:09:35 +0100

    add animation

--- brickyMartin/scripts/paddle.gd
+++ brickyMartin/scripts/paddle.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 const SPEED = 1000.0
 var paddleVelocity := Vector2.ZERO
 var lockedYAxis: float
+var is_active := true
+
+@onready var animatedSprite = $AnimatedSprite2D
 
 func _ready() -> void:
 	lockedYAxis = global_position.y
@@ -24,3 +27,16 @@ func _physics_process(delta: float) -> void:
 
 	paddleVelocity = (global_position - prev_pos) / delta
 	paddleVelocity.y = 0
+
+func interact_with_ball() -> void:
+	animatedSprite.play("hit")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	is_active = false
+	animatedSprite.play("hit")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	is_active = false
+	animatedSprite.play("idle")
