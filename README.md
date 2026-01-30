17a942666516bdea0de5ba7803ececfbe9621f36
Baumel Márton <marcibaumel@gmail.com>
2026-01-30 12:23:46 +0100

    add sfx

--- README.md
+++ README.md
@@ -4,21 +4,12 @@ This is a repository about january HEJ game jam.
 
 [![Export status](https://github.com/marcibaumel/HEJ_Brick/actions/workflows/create-build.yml/badge.svg)](https://github.com/marcibaumel/HEJ_Brick/actions/workflows/create-build.yml)
 
-## Description
 
-### THEME: 
-  
-## Tasks:
-
-
-## Disclaimer
-
-- This repository not contains any AI generated content <img src="./asset/sadhamstergirl.gif" width="20">
-
-## What I used
+## Assets:
 
 - [Sad hamster girl gif](https://tenor.com/hu/view/sadhamstergirl-gif-4231717927828306245)
 - [Make A Video Game in 30 Minutes](https://www.youtube.com/watch?v=IYjRhcOCFDo)
 - [Menu tutorial](https://www.youtube.com/watch?v=zHYkcJyE52g)
 - [Grape Soda Font](https://www.dafont.com/grapesoda-2.font)
 - [Music](https://opengameart.org/content/hungry-dino-9-chiptune-tracks-10-sfx)
+- [Destroy sound](https://opengameart.org/content/crash-collision)
--- brickyMartin/scripts/brick.gd
+++ brickyMartin/scripts/brick.gd
@@ -5,6 +5,10 @@ extends StaticBody2D
 @export var enableAbilitiyDoubleBall: bool = false
 @export var exp_scene: PackedScene = preload("res://scenes/exp.tscn")
 
+@onready var destroySound = $DestroySound
+
+@onready var animation = $AnimatedSprite2D
+
 var baseColour = "#ffffff"
 var is_destroyed = false
 
@@ -12,7 +16,6 @@ func _ready() -> void:
 	if enableDoubleHealth:
 		$Sprite2D.modulate = "#cf6fe2"
 		health = 2
-
 
 func hit() -> void:
 	health -= 1
@@ -26,6 +29,7 @@ func destroyBrick() -> void:
 	spawn_exp()
 
 	$Sprite2D.visible = false
+	animation.visible = false
 	$CollisionShape2D.disabled = true
 	$CPUParticles2D.emitting = true
 	
@@ -33,6 +37,7 @@ func destroyBrick() -> void:
 	set_collision_layer(0)
 	set_collision_mask(0)
 		
+	destroySound.play()
 	await get_tree().create_timer(0.5).timeout
 	queue_free()
 
@@ -43,8 +48,8 @@ func spawn_exp() -> void:
 	get_parent().add_child(drop)
 	drop.global_position = global_position
 
-    # CHANGED: randf() < 0.2 means a 20% chance for heal. 
-    # If you leave it at 1.0, it will ALWAYS be a heal.
+	# CHANGED: randf() < 0.2 means a 20% chance for heal. 
+	# If you leave it at 1.0, it will ALWAYS be a heal.
 	if randf() < 0.5:
 		drop.init_item("heal", Color.CHARTREUSE)
 	else:
@@ -53,4 +58,5 @@ func spawn_exp() -> void:
 
 func _on_paddle_detect_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Paddle"):
+		destroySound.play()
 		queue_free()
